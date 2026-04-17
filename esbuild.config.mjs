d42c4270331b913ec0d1095468c66b71796fa3d9
Eugene Archibald <eugene.archibald+gh@gmail.com>
2026-04-17 13:42:57 -0700

    chore: scaffold obsidian plugin toolchain

---
+++ esbuild.config.mjs
@@ -0,0 +1,25 @@
+import esbuild from "esbuild";
+import process from "process";
+import builtins from "builtin-modules";
+
+const prod = process.argv[2] === "production";
+
+const ctx = await esbuild.context({
+  entryPoints: ["src/main.ts"],
+  bundle: true,
+  external: ["obsidian", "electron", "keytar", ...builtins],
+  format: "cjs",
+  target: "es2022",
+  logLevel: "info",
+  sourcemap: prod ? false : "inline",
+  treeShaking: true,
+  outfile: "main.js",
+  platform: "node",
+});
+
+if (prod) {
+  await ctx.rebuild();
+  await ctx.dispose();
+} else {
+  await ctx.watch();
+}
