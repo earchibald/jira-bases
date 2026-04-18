01a7ab3bd308df17ef8caa008bdeca727cff861d
Eugene Archibald <eugene.archibald+gh@gmail.com>
2026-04-17 20:10:22 -0700

    chore: add jsdom for upcoming view tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,5 +4,8 @@ export default defineConfig({
   test: {
     environment: "node",
     include: ["src/**/*.test.ts"],
+    environmentMatchGlobs: [
+      ["src/**/issue-preview-view.test.ts", "jsdom"],
+    ],
   },
 });
